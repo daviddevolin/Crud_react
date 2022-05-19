744f2192c7c5a8ccb92a0ddd2098ccb86d83cde6
david santos <56547716+daviddevolin@users.noreply.github.com>
2022-05-19 11:54:11 -0300

    preparando ambiente e iniciando a aplicação

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Museu</h1>
+      <div className="Home" >
+        <label>Museu Name:</label>
+        <input type="text" name="Museuname" />
+        <label>Museu Rivew:</label>
+        <input type="text" name="Museurivew" />
+        <button>Submit</button>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
