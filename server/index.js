b4b8d069de23e038649920175521ddfcadb25823
david santos <56547716+daviddevolin@users.noreply.github.com>
2022-05-30 12:18:40 -0300

    criando conexção com o BD

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
-const express =require('express');
-
+const express =require("express");
 const app = express();
+const mysql = require('mysql');
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "crudmuseu",
+});
 
 app.get('/',(req, res) =>{
-    res.send("OPAAAA")
-})
+
+    let SQL = "INSERT INTO museus (name, museuReview) VALUES('troncos e raizes', 'museu indigena');";
+    db.query(SQL,(err,result)=>{
+
+        res.send("OPPPA");
+        console.log(err);
+        
+    })
+});
+
+
 
 app.listen(3001, () => {
     console.log("Server Running")
-}) 
+});
