37fb3ce6c85aa09cba1df9b4e443ad132bb2fdad
david santos <56547716+daviddevolin@users.noreply.github.com>
2022-05-31 16:10:06 -0300

    criando as funções update e delete

--- client/src/App.js
+++ client/src/App.js
@@ -6,39 +6,66 @@ function App() {
   const [museuName, setMuseuName] = useState("");
   const [museuReview, setMuseuReview] = useState("");
   const [museuReviewList, setMuseuList] = useState([]);
+  const [newReview, setNewReview] = useState("");
+
+
   useEffect(()=>{
     Axios.get('http://localhost:3001/api/get').then((Response)=>{
       setMuseuList(Response.data)
-    })
-  }, [])
+    });
+  }, []);
 
-  const SubmitReview = ()=>{
+  const submitReview = ()=>{
     Axios.post('http://localhost:3001/api/insert',{
       museuName:museuName,
       museuReview:museuReview
-    }).then(()=>{
-      alert ('inserido');
     });
 
+    setMuseuList([
+      ...museuReviewList,
+      { museuName : museuName ,museuReview: museuReview},
+    ]);
   };
+
+  const deleteReview = (museuName)=> {
+    Axios.delete(`http://localhost:3001/api/delete/${museuName}`);
+  };
+
+  const updateReview = (museuName)=> {
+    Axios.put("http://localhost:3001/api/update",{
+      museuName:museuName,
+      review: newReview
+    });
+    setNewReview("")
+  };
+
 
   return (
     <div className="App">
       <h1>Museu</h1>
       <div className="home" >
+        
         <label>Museu Name:</label>
         <input type="text" name="museuName" onChange={(e)=>{
           setMuseuName(e.target.value)
         }} />
+
         <label>Museu Rivew:</label>
         <input type="text" name="museuReview" onChange={(e)=>{
           setMuseuReview(e.target.value)
         }} />
-        <button onClick={SubmitReview}>Submit</button>
+
+        <button onClick={submitReview}>Submit</button>
         {museuReviewList.map((val)=>{
-          return <h1>
-            Museu Name: {val.museuName} | Museu Review: {val.museuReview}
-          </h1>
+          return <div  className='card'>
+            <h1>{val.museuName}</h1>  
+            <p>{val.museuReview}</p>
+            <button onClick={() => deleteReview(val.museuName)}> delete </button>
+            <input type="text"  id="updateInput" onChange={(e) =>{
+              setNewReview (e.target.value)
+            }}/>
+            <button onClick={()=>{updateReview(val.museuName)}}>update</button>
+          </div>
         })}
 
       </div>
--- server/index.js
+++ server/index.js
@@ -18,12 +18,12 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/api/get',(req,res)=>{
-    let SQL = "SELECT * FROM museus";
+    const SQL = "SELECT * FROM museus";
     db.query(SQL,(err,result)=>{
         res.send(result)
         
     });
-})
+});
 
 app.post('/api/insert',(req, res) =>{
 
@@ -31,8 +31,8 @@ app.post('/api/insert',(req, res) =>{
     const museuReview = req.body.museuReview;
     
 
-    let SQL = "INSERT INTO museus (museuName, museuReview) VALUES(?,?)";
-    db.query(SQL, [museuName,museuReview],(err,result)=>{
+    let sqlInsert = "INSERT INTO museus (museuName, museuReview) VALUES(?,?)";
+    db.query(sqlInsert, [museuName,museuReview],(err,result)=>{
         console.log(err);
         
     });
@@ -43,4 +43,22 @@ app.post('/api/insert',(req, res) =>{
 
 app.listen(3001, () => {
     console.log("Server Running")
+});
+
+app.delete('/api/delete/:museuName', (req,res)=>{
+    const name = req.params.museuName
+    const sqlDelete = "DELETE FROM museus WHERE museuName = ?";
+    db.query(sqlDelete, name, (err,result)=>{
+        if(err) console.log(err)
+    });
+});
+
+
+app.put('/api/update', (req,res)=>{
+    const name = req.body.museuName;
+    const review = req.body.review;
+    const sqlUpdate = "UPDATE  museus SET museuReview = ? WHERE museuName =? ";
+    db.query(sqlUpdate, [review,name], (err,result)=>{
+        if(err) console.log(err)
+    });
 });
